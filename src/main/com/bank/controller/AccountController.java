31417445e97097bac0ddf6c785ad6245d45d9467
team2_initial <ninenutk@gmail.com>
2017-08-22 13:52:29 +0700

    fix datasource and make spring boot works

--- src/main/com/bank/controller/AccountController.java
+++ src/main/com/bank/controller/AccountController.java
@@ -2,7 +2,6 @@ package com.bank.controller;
 
 import com.bank.domain.Account;
 import com.bank.repository.AccountRepository;
-import com.bank.repository.internal.JdbcAccountRepository;
 import com.bank.repository.internal.SimpleAccountRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.sql.DataSource;
 import java.util.List;
 
-@Controller
+@RestController
 public class AccountController {
 
     @RequestMapping(value = "/getAccountListByUserId", method = {RequestMethod.GET}, produces = "application/json")

ab4c5b4c994c41e5b7fce2048f72b5032cfb665e
Prang <pr.3ung@gmail.com>
2017-08-23 10:09:08 +0700

    Fix Jenkins failure

--- src/main/com/bank/service/internal/VerifyTransferService.java
+++ src/main/com/bank/service/internal/VerifyTransferService.java
@@ -5,21 +5,25 @@ import com.bank.domain.InsufficientFundsException;
 import com.bank.domain.TransferReceipt;
 import com.bank.domain.VerifyTransferBean;
 import com.bank.dto.VerifyTransferResponseDTO;
+import com.bank.repository.AccountRepository;
 import com.bank.repository.internal.JdbcAccountRepository;
 import com.bank.service.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
+
 @Service
 public class VerifyTransferService implements TransferService{
 
     @Autowired
-    private JdbcAccountRepository accountRepository;
+    DataSource dataSource;
 
     @Override
     public VerifyTransferResponseDTO verifyTransfer(VerifyTransferBean transferBean) {
         String accountNumber = transferBean.getToAccount().getAccountNumber();
-        Account account = accountRepository.findByAccountNumber(accountNumber);
+        AccountRepository accountRep = new JdbcAccountRepository(dataSource);
+        Account account = accountRep.findByAccountNumber(accountNumber);
         VerifyTransferResponseDTO responseDTO = new VerifyTransferResponseDTO();
         if (account != null) {
             responseDTO.setValid(true);

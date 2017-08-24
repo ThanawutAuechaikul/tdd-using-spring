62b230fa907dea476c2115d6a5dcd3231445175d
team2_initial <ninenutk@gmail.com>
2017-08-24 14:50:29 +0700

    Update constant value

--- src/main/com/bank/com/bank/constant/Constant.java
+++ src/main/com/bank/com/bank/constant/Constant.java
@@ -1,6 +1,6 @@
 package com.bank.com.bank.constant;
 
 public class Constant {
-    public static final String SUCCESS = "Success";
-    public static final String FAILED = "Failed";
+    public static final String SUCCESS = "SUCCESS";
+    public static final String FAILED = "FAILED";
 }
